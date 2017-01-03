001b28b564a173b17ae65036646e7f56a9ae10e1
vitiok <leschuk@ex.ua>
2017-01-03 17:41:14 +0200

    Disabled chat route

--- app/Http/Controllers/ChatController.php
+++ app/Http/Controllers/ChatController.php
@@ -15,22 +15,6 @@ class ChatController extends Controller
 
     const DEFAULT_CHAT_CHANNEL = 'chat';
 
-    public function __construct()
-    {
-    }
-
-    public function index()
-    {
-//        if(!$this->user)
-//        {
-//            return redirect('auth/github?redirect=/chat');
-//        }
-
-//        dd($this->user);
-
-        return view('chat', ['chatChannel' => self::DEFAULT_CHAT_CHANNEL]);
-    }
-
     public function store(Request $request)
     {
         $user = auth()->user();
